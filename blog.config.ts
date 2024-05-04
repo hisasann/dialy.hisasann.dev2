91a569d1117834b1f9e846c64fdf5f169fa69475
hisamatsu yoshiyuki <hisasann25@gmail.com>
2024-05-05 03:36:45 +0900

    chore(block.config): remove unnecessary props

--- blog.config.ts
+++ blog.config.ts
@@ -7,17 +7,15 @@ const BLOG = {
   lang: 'ja-JP', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#030a12cf', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 10,
+  postsPerPage: 30,
   sortByDate: false,
-  showAbout: true,
-  showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://hisasann.dev/assets/images/hisasann_400x400.jpg', // The link to generate OG image, don't end with a slash
+  note: 'https://note.com/hisasann', // leave this empty unless you want to deploy Nobelium in a folder
   socialLink: 'https://twitter.com/hisasann',
+  instagram: 'https://www.instagram.com/hisasann/',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '', // Remove the value or replace it with your own google site verification code
@@ -33,26 +31,6 @@ const BLOG = {
     },
     gaConfig: {
       measurementId: '', // e.g: G-XXXXXXXXXX
-    },
-  },
-  comment: {
-    // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
-    gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
-      admin: [],
-      clientID: '',
-      clientSecret: '',
-      distractionFreeMode: false,
-    },
-    utterancesConfig: {
-      repo: '',
-    },
-    cusdisConfig: {
-      appId: '', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
-      scriptSrc: 'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
     },
   },
   isProd: process.env.VERCEL_ENV === 'production', // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
